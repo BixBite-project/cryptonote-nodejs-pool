647a1e163ba06448d28f7c5c0b019caacc74d7a8
BixBite-project <38261741+BixBite-project@users.noreply.github.com>
2018-06-21 19:51:40 +0500

    mining calculator improvements

---
+++ website/config.js
@@ -0,0 +1,15 @@
+var api = "https://pool.bixbite.pro:5000";
+var poolHost = "pool.bixbite.pro";
+
+var email = "team@bitxbite.pro";
+var telegram = "https://t.me/bixbitecoin";
+var discord = null;
+
+// var marketCurrencies = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR", "{symbol}-RUR"];
+var marketCurrencies = ["{symbol}-BTC", "{symbol}-USD"];
+
+var blockchainExplorer = "http://explorer.bixbite.pro/block/{id}";
+var transactionExplorer = "http://explorer.bixbite.pro/tx/{id}";
+
+var themeCss = "themes/default.css";
+var defaultLang = 'en';
